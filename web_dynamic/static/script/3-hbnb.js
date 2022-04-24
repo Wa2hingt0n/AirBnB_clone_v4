05b702c4fe8c866a127ae6410cd3aa33b84d6ade
dnjoe96 <donjoedbest@gmail.com>
2022-04-24 18:36:48 +0100

    Modify AJAX request

--- web_dynamic/static/script/3-hbnb.js
+++ web_dynamic/static/script/3-hbnb.js
@@ -23,17 +23,16 @@ $(document).ready(function () {
     $('.amenities h4').text(amList.join(', '));
   });
 
-  // POST request to http://0.0.0.0:5001/api/v1/places_search/
   $.ajax({
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    headers: {
-      contentType: 'application/json'
-    },
+     contentType: 'application/json; charset=utf-8',
+    dataType: "json",
     method: 'POST',
-    data: {},
+//    data: JSON.stringify({}),
+    data: '{}',
     success: function (data) {
       for (let i = 0; i < data.length; i++) {
-        $('section.places').append('<article> <div class="title_box"> <h2>' + data[i].name + '</h2> <div class="price_by_night">' + data[i].price_by_night + '</div> </div> <div class="information"> <div class="max_guest">' + data[i].max_guest + 'Guests</div> <div class="number_rooms">' + data[i].number_rooms + 'Rooms</div> <div class="number_bathrooms">' + data[i].number_bathrooms + '</div> </div> <!-- <div class="user"> --> <!-- </div> --> <div class="description">' + data[i].description + '</div> </article>'
+        $('section.places').append('<article> <div class="title_box"> <h2>' + data[i].name + '</h2> <div class="price_by_night">' + data[i].price_by_night + '</div> </div> <div class="information"> <div class="max_guest">' + data[i].max_guest + ' Guests</div> <div class="number_rooms">' + data[i].number_rooms + ' Rooms</div> <div class="number_bathrooms">' + data[i].number_bathrooms + ' Bathrooms </div> </div> <!-- <div class="user"> --> <!-- </div> --> <div class="description">' + data[i].description + '</div> </article>'
         );
       }
     }
